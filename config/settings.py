c259bb8e5f23a10eb66e0c2625b399afcd8f3ca1
bobojonov-sobirjon <sobirbobojonov05763104@gmail.com>
2026-04-01 11:00:23 +0500

    changed api to swagger

--- config/settings.py
+++ config/settings.py
@@ -458,6 +458,7 @@ SPECTACULAR_SETTINGS = {
         'hideHostname': True,
     },
     'SERVERS': [
+        {'url': 'https://85.198.101.179:8005', 'description': 'Production server'},
         {'url': 'http://localhost:8000', 'description': 'Development server'},
     ],
     'TAGS': [
